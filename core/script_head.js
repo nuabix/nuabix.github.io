4f4d2200f52a6fbed288efc939962673dab4c518
Nuabix <132663353+nuabix@users.noreply.github.com>
2023-05-06 13:01:16 +0900

    Update script_head.js

--- core/script_head.js
+++ core/script_head.js
@@ -1,40 +1,57 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      /* Hide all elements, except for the element with ID 'myContent' */
-      :not(#myContent):fullscreen {
-        display: none;
-      }
+window.onload = function() {
+  document.querySelector('.console-input-field').focus();
+  var consoleHeader = document.getElementById("console-header");
+  var console = document.getElementById("console");
+  var offsetX = 0;
+  var offsetY = 0;
+  var isDragging = false;
+  var isFullscreen = false;
 
-      /* Make 'myContent' take up the whole screen */
-      #myContent:fullscreen {
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Fullscreen Demo</h1>
-    <button onclick="openFullscreen();">Go Fullscreen</button>
+  consoleHeader.addEventListener("mousedown", function(e) {
+    offsetX = e.clientX - console.offsetLeft;
+    offsetY = e.clientY - console.offsetTop;
+    isDragging = true;
+  });
 
-    <div id="myContent">
-      <p>This is some content that will be visible in fullscreen mode.</p>
-    </div>
+  document.addEventListener("mousemove", function(e) {
+    if (isDragging) {
+      console.style.left = (e.clientX - offsetX) + "px";
+      console.style.top = (e.clientY - offsetY) + "px";
+    }
+  });
 
-    <script>
-      function openFullscreen() {
-        var elem = document.documentElement;
-        if (elem.requestFullscreen) {
-          elem.requestFullscreen();
-        } else if (elem.mozRequestFullScreen) { /* Firefox */
-          elem.mozRequestFullScreen();
-        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-          elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { /* IE/Edge */
-          elem.msRequestFullscreen();
-        }
+  document.addEventListener("mouseup", function(e) {
+    isDragging = false;
+  });
+
+  var consoleMaximize = document.querySelector('.console-maximize');
+  consoleMaximize.addEventListener('click', function() {
+    if (!isFullscreen) {
+      if (console.requestFullscreen) {
+        console.requestFullscreen();
+      } else if (console.webkitRequestFullscreen) { /* Safari */
+        console.webkitRequestFullscreen();
+      } else if (console.msRequestFullscreen) { /* IE11 */
+        console.msRequestFullscreen();
       }
-    </script>
-  </body>
-</html>
+      isFullscreen = true;
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+      }
+      isFullscreen = false;
+    }
+  });
+
+  document.addEventListener('fullscreenchange', function(e) {
+    if (!document.fullscreenElement) {
+      console.style.left = "";
+      console.style.top = "";
+      isFullscreen = false;
+    }
+  });
+}
