34deef0ab3616600ec62dbfc0830eb66b4356d77
Nuabix <132663353+nuabix@users.noreply.github.com>
2024-01-03 19:02:56 +0900

    Update run.js

--- core/run.js
+++ core/run.js
@@ -1,13 +1,12 @@
 const API_BASE = config.apikey;
 const URI = `${API_BASE}/v1/completions`;
 
-const char = bot.char;
-const CharInfo = bot.CharInfo;
-const FirstMsg = bot.FirstMsg;
-
-const user = "User";
-
 const MAX_TOKENS_LIMIT = 6000;
+
+function formatString(str) {
+    str = str.replace(/^\n+|\n+$/g, '');
+    return str.replace(/\n\s*\n/g, '\n\n');
+}
 
 function calculateTotalTokens() {
     return log.reduce((totalTokens, logEntry) => totalTokens + logEntry.length, 0);
@@ -20,14 +19,14 @@ function manageMemory() {
     }
 }
 
-const log = [`${char}:\n${FirstMsg}\n`];
+const log = [`${char}:\n${firstmsg}\n`];
 async function run(text) {
     const request = {
-        prompt: `${CharInfo}\n\n${log.join("")}${user}:\n${text}\n${char}:\n`,
-        stop: [`${user}:`, `${char}:`],
+        prompt: `### Instruction:${inst}\n\n### ${char}'s Information:\n${charinfo}\n\n### Chat start:\n${log.join("")}${user}:\n${text}\n${char}:\n`,
+        stop: [`${user}:`, `${char}:`,`###`],
         temperature: 0.9,
         top_p: 0.9,
-        max_tokens: 250
+        max_tokens: 300
     };
 
     try {
@@ -43,19 +42,16 @@ async function run(text) {
             throw new Error(`Failed to fetch data. Status: ${response.status}`);
         }
 
-        const result = (await response.json()).choices[0].text;
+        let result = (await response.json()).choices[0].text;
+        result = formatString(result);
         log.push(`${user}:\n${text}\n${char}:\n${result}\n`);
         manageMemory();
-        displayMessage(result, "ai-message");
-
         if (ttsEnabled) {
             speakText(result);
         }
+        result = result.replace(/\n/g, '<br>');
+        displayMessage(result, "ai-message");
     } catch (error) {
         console.error("Error:", error.message);
     }
-}
-
-function addLineBreaks(message) {
-    return message.replace(/(\d+\.\s)/g, "<br><br>$1");
 }
